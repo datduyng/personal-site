5f2ed87b1ed54f4b6ada769f16f3cf6ae43beed9
Dominic Nguyen <35666615+datduyng@users.noreply.github.com>
2022-04-09 23:47:33 +0000

    feat: change featured project to DemoShot

--- components/home-page-cards/featured-project-card.tsx
+++ components/home-page-cards/featured-project-card.tsx
@@ -6,9 +6,12 @@ export default function FeaturedProjectCard() {
       🛠️
     </div>
     <p>
-      {`I am working on Scriptbar a browser spotlight tool that allow developer to
-      execute handy snippet with a few shortcut. Developer can explore and download
-      snippets from SnippetStore`}
+      {`I am working on `}
+      <a className="underline" href="https://domnguyen.qstack.us/notes/e21a10d11e404241b4b9495675603097-demoshot-tool-to-create-3d-transform-image-shot" target={'_blank'}>
+        DemoShot
+      </a>
+      {`, a web tool that allow developer to create 3d transformed product thumbnail 
+      and cover page with browser border for twitter, producthunt, chrome web store, social media post.`}
     </p>
   </HomePageCard>)
 }
