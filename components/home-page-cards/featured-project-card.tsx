74596272df990b3bca051b9c3ba5b79f9727462c
Dominic Nguyen <35666615+datduyng@users.noreply.github.com>
2022-04-09 23:59:06 +0000

    fix grammar

--- components/home-page-cards/featured-project-card.tsx
+++ components/home-page-cards/featured-project-card.tsx
@@ -10,8 +10,8 @@ export default function FeaturedProjectCard() {
       <a className="underline" href="https://domnguyen.qstack.us/notes/e21a10d11e404241b4b9495675603097-demoshot-tool-to-create-3d-transform-image-shot" target={'_blank'}>
         DemoShot
       </a>
-      {`, a web tool that allow developer to create 3d transformed product thumbnail 
-      and cover page with browser border for twitter, producthunt, chrome web store, social media post.`}
+      {`, a tool that allow developers to create 3d transformed product thumbnail 
+      and cover page for twitter, producthunt, chrome web store, and social media post.`}
     </p>
   </HomePageCard>)
 }
