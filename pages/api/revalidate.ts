74bae18e4aab27880e553b70798e52042acd47d0
datduyng <datnguyen5653@gmail.com>
2023-11-04 02:51:31 -0700

    refactor

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,22 +1,28 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+
 type Data = {
   errorMessage?: string;
   revalidated?: boolean;
 };
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>,
-) {
+async function revalidateOrThrow(res: NextApiResponse<Data>, route: string): Promise<void> {
+  try {
+    await res.revalidate(route);
+  } catch (error) {
+    throw new Error('Error revalidating');
+  }
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   // Dear hacker: good luck guessing this.
   if (req.query.secret !== process.env.REVALIDATE_SECRET_TOKEN) {
-    return res.status(401).json({ errorMessage: 'Invalid token' })
+    return res.status(401).json({ errorMessage: 'Invalid token' });
   }
 
   try {
-    await res.revalidate(req.query?.route as string || '')
-    return res.json({ revalidated: true })
-  } catch (err) {
-    return res.status(500).json({ errorMessage: 'Error revalidating' })
+    await revalidateOrThrow(res, req.query?.route as string || '');
+    return res.json({ revalidated: true });
+  } catch (error: any) {
+    return res.status(500).json({ errorMessage: error.message });
   }
 }
