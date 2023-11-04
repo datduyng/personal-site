5797e3e58b1645245890253aef02e126ea0f74c4
datduyng <datnguyen5653@gmail.com>
2023-11-04 03:01:21 -0700

    refactor

--- pages/shortener.tsx
+++ pages/shortener.tsx
@@ -1,9 +1,17 @@
 import { NextPage } from 'next';
+import { useEffect, useState } from 'react';
 
 const Shortener: NextPage = () => {
-  if (typeof window === 'undefined') return null;
+  const [host, setHost] = useState<string | null>(null);
 
-  const host = window.location.host;
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setHost(window.location.host);
+    }
+  }, []);
+
+  if (!host) return null;
+
   return (
     <iframe src={`https://notion-url-shortener-dom.vercel.app?host=${window.location.protocol
       }//${host}/l`} allow="clipboard-read; clipboard-write" style={{ width: '100%', height: '100vh' }} />
